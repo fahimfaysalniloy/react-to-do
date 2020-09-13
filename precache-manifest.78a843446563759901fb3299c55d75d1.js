5c1b693ba101e72d1ec199d6c16c69b9200bc876
fahim faysal niloy <fahimfaysal.buetbd@gmail.com>
2020-09-13 14:32:25 +0600

    Updates

---
+++ precache-manifest.78a843446563759901fb3299c55d75d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b21872d8e52ae6f035fcae51afe88b",
+    "url": "/github.io/react-to-do/index.html"
+  },
+  {
+    "revision": "065bd482687d685472c5",
+    "url": "/github.io/react-to-do/static/css/main.d6e90e39.chunk.css"
+  },
+  {
+    "revision": "91e89da0c999af19f2b4",
+    "url": "/github.io/react-to-do/static/js/2.b3c74d16.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github.io/react-to-do/static/js/2.b3c74d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065bd482687d685472c5",
+    "url": "/github.io/react-to-do/static/js/main.db0e8602.chunk.js"
+  },
+  {
+    "revision": "4b5afe5b381f9bf5b2e3",
+    "url": "/github.io/react-to-do/static/js/runtime-main.08b9e6d9.js"
+  }
+]);
